98fd7eaf677f9b8b0b7b8b8fff58a80161e7476c
Valentin <kvmisis@gmail.com>
2014-04-24 01:57:03 +0400

    Fix long inc subseq

--- cpp/longest_increasing_subsequence.cpp
+++ cpp/longest_increasing_subsequence.cpp
@@ -1,12 +1,14 @@
-const int N = 100000;
-const int INF = (int)(1e9);
-int a[N], d[N + 1];
+const int N = 100000; // min <= a[i] <= max
+int a[N], d[N + 1], len[N], order[N];
 
-void solve(int n) {
-  fill(d, d + n + 1, INF);
-  d[0] = -INF;
-  forn (i, n) {
-    int j = upper_bound(d, d + n, a[i]) - d;
-    if (d[j-1] < /* <= */ a[i] && a[i] < d[j]) d[j] = a[i];
+void solve(int n, int min, int max, int& ans) {
+  fill(d, d+n+1, min-1); d[0] = max+1;
+  for (int i = n-1; i >= 0; --i) {
+    int j = lower_bound(d, d+n+1, a[i], greater<int>()) - d;
+    d[j] = a[i], len[i] = j;
   }
+  ans = lower_bound(d, d+n+1, min-1, greater<int>()) - d - 1;
+  for (int i = 0, goal = ans, last = min-1; goal != 0; ++i)
+    if (len[i] == goal && last < a[i])
+      order[--goal] = i, last = a[i];
 }
