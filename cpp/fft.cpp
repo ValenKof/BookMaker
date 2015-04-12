90cfe5e8600a0c4ee45800205a2aaf30afe8eed5
Valentine Kofman <yndx.valenkof@gmail.com>
2015-04-13 02:17:25 +0300

    edit for quick copy-paste

--- cpp/fft.cpp
+++ cpp/fft.cpp
@@ -1,4 +1,3 @@
-typedef complex<double> base; // or struct { double re, im }
 // int MOD=1051721729, GEN=330, REV=rev(GEN,MOD), POWER=1<<20;
 // binPow(GEN,POWER,MOD) = 1, binPow(GEN,x,MOD) != 1  0<x<POWER
 
@@ -7,7 +6,31 @@ typedef complex<double> base; // or struct { double re, im }
 // FFT_B[i] = base(0,1) * (!FFT_C[(n-i)&(n-1)] - FFT_C[i]) / 2.
 // !(A + i * B) = A - i * B.
 
-void fft(vector<base>& a, bool invert) {
+// typedef complex<double> base;
+
+struct base
+{
+  double re, im;
+  explicit base(double re = 0, double im = 0) : re(re), im(im) { }
+};
+
+inline base operator!(const base& a)
+{ return base(a.re, -a.im); }
+
+inline base operator+(const base& a, const base& b)
+{ return base(a.re + b.re, a.im + b.im); }
+
+inline base operator-(const base& a, const base& b)
+{ return base(a.re - b.re, a.im - b.im); }
+
+inline base operator*(const base& a, const base& b)
+{ return base(a.re * b.re - a.im * b.im, a.re * b.im + a.im * b.re); }
+
+inline base operator/(const base& a, const double& b)
+{ return base(a.re / b, a.im / b); }
+
+void fft(vector<base>& a, bool invert)
+{
   int n = sz(a);
   for (int i = 1, j = 0; i < n; ++i) {
     int bit = n >> 1;
@@ -34,10 +57,11 @@ void fft(vector<base>& a, bool invert) {
       }
     }
   }
-  if (invert) forn (i, n) a[i] /= n; // ... %MOD
+  if (invert) forn (i, n) a[i] = a[i] / n; // ... %MOD
 }
 
-base gen() {
+base gen()
+{
   for (base x = 1; x < MOD; ++x) {
     if (binPow(x, POWER, MOD) == 1) {
       bool ok = true;
@@ -57,11 +81,11 @@ base gen() {
   return -1;
 }
 
-void tr(vector<vector<base> >& a) {
-  forn (i, sz(a)) forn (j, i) swap(a[i][j], a[j][i]);
-}
+void tr(vector<vector<base> >& a)
+{ forn (i, sz(a)) forn (j, i) swap(a[i][j], a[j][i]); }
 
-void fft(vector<vector<base> >& a, bool invert) {
+void fft(vector<vector<base> >& a, bool invert)
+{
   if (invert) tr(a);
   forn (i, sz(a)) fft(a[i], invert);
   tr(a);
